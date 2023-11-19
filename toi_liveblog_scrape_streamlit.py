e706f52f3d773d493330065d481e9774cd3622e0
steven-rothstein <2152613+steven-rothstein@users.noreply.github.com>
2023-11-19 17:47:57 -0500

    headers again

--- toi_liveblog_scrape_streamlit.py
+++ toi_liveblog_scrape_streamlit.py
@@ -22,7 +22,14 @@ def generate_url_request(url):
         Request(
             url,
             headers={
-                "User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3"
+                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) ",
+                #"AppleWebKit/537.11 (KHTML, like Gecko) "
+                #"Chrome/23.0.1271.64 Safari/537.11",
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+                "Accept-Charset": "ISO-8859-1,utf-8;q=0.7,*;q=0.3",
+                "Accept-Encoding": "none",
+                "Accept-Language": "en-US,en;q=0.8",
+                "Connection": "keep-alive",
             },
         ),
         timeout=10,
