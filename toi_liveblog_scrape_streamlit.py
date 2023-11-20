111d90152e8f7f61441bf9a37471fb15724586da
steven-rothstein <2152613+steven-rothstein@users.noreply.github.com>
2023-11-19 19:18:36 -0500

    Update toi_liveblog_scrape_streamlit.py

--- toi_liveblog_scrape_streamlit.py
+++ toi_liveblog_scrape_streamlit.py
@@ -18,20 +18,7 @@ def generate_scrape_url(ts_arg):
 
 
 def generate_url_request(url):
-    return urlopen(
-        Request(
-            url,
-            headers={
-                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11",
-                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-                "Accept-Charset": "ISO-8859-1,utf-8;q=0.7,*;q=0.3",
-                "Accept-Encoding": "none",
-                "Accept-Language": "en-US,en;q=0.8",
-                "Connection": "keep-alive",
-            },
-        ),
-        timeout=10,
-    )
+    return urlopen(Request(url, headers={"User-Agent": "Mozilla/5.0"}))
 
 
 def scrape_liveblog(ts_arg):
